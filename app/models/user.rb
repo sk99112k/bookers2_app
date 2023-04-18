4574a708a46974239f3fcb179c52ca9e8af13382
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-18 04:48:01 +0000

    good

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
+  
   def get_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
   get 'home/about' => 'homes#about'
-  resources :books, only: [:new,:create,:index,:edit,:update,:show,:destroy]
+  resources :books, only: [:new,:create,:index,:edit,:update,:show,:destroy] do
+    resource :favorites, only:[:create,:destroy]
+  end
   resources :users, only: [:show,:index,:edit,:update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
