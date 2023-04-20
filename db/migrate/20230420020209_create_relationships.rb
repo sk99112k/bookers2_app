8c84c5ece112dbade306d22ac83698f99f332dff
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-20 06:45:04 +0000

    follow 0420 15:30

--- db/migrate/20230420020209_create_relationships.rb
+++ db/migrate/20230420020209_create_relationships.rb
@@ -1,8 +1,12 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
+
+      add_index :relationships, :followed_id
+      add_index :relationships, %i[follower_id followed_id], unique: true
 
       t.timestamps
     end
