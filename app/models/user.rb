9c3a0a83038885f0181e397f0e6b0a2695a0df3e
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-22 04:53:00 +0000

    finish follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+  #userがフォローしてる人
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followers, through: :active_relationships, source: :follower
+  has_many :followers, through: :active_relationships, source: :followed
+  #userをフォローしてる人
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :passive_relationships, source: :followed
-  
+  has_many :followings, through: :passive_relationships, source: :follower
+
   has_one_attached :profile_image
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
@@ -24,17 +25,17 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def follow(user)
-    relationships.create(followed_id: user.id)
-  end 
-  
+    active_relationships.create(followed_id: user.id)
+  end
+
   def unfollow(user)
-    relationships.find_by(followed_id: user.id).destroy
-  end 
-  
-  def following?(user)
-    followings.include?(user)
-  end 
-  
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def follow?(user)
+    followers.include?(user)
+  end
+
 end
