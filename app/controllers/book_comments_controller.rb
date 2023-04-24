1fffd994bbfec568a5d21e6128f12ec4e58bb744
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-24 12:36:56 +0000

    0424 comm 21:30

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,13 @@
 class BookCommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    unless @comment.save
+      render 'error'
+    end
     #redirect_to request.referer
   end
 
