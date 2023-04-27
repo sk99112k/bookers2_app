419c578efeb563d0ef0943f07b44d688e7a859f3
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-27 13:30:03 +0000

    0427 22:30 guestuser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
@@ -33,6 +34,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name,:profile_image,:introduction)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notice: "guestuserはプロフィール編集画面へ遷移できません。"
+    end
   end
 
 end
